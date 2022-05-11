35c7f1dbc1d6cc628917540bacf91ffed7827483
Juan Pedro <juan.dominguez2763@gmail.alumnos.udg.mx>
2022-05-11 16:57:50 -0500

    Estructura inicial del proyecto, login y register incluidos

---
+++ resources/views/Login.php
@@ -0,0 +1,35 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="container-login">
+            <div class="col-left">
+                <h1>Ingresar</h1>
+                <form class="form-group" method="GET" action="register.html">
+                    <input type="text" class="input " placeholder="Correo">
+                    <input type="password" class="input" placeholder="Contraseña">
+                    <input type="submit" class="btn" value="Ingresar">
+                
+                        <a href="">Ingresar como invitado</a>
+                </form> 
+            </div>
+            <div class="col-right">
+                <div class="content-text">
+                    <h2>Hola de nuevo</h2>
+                    <p>Ingresa el correo y la contraseña para iniciar sesion</p>
+                    <p>Si no tienes una cuenta puedes crear una aqui</p>
+                    <button id="btn-register"><a href="./register.html">Registrar</a></button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
---
+++ resources/views/Register.php
@@ -0,0 +1,34 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+        <div class="container-register ">
+            <div class="col-left">
+                <h1>Registrar</h1>
+                <form class="form-group">
+                    <input type="text" class="input" placeholder="Nombre completo">
+                    <input type="text" class="input " placeholder="Correo">
+                    <input type="password" class="input" placeholder="Contraseña">
+                    <input type="submit" class="btn" value="Registrarse">
+                </form> 
+            </div>
+            <div class="col-right border-radius">
+                <div class="content-text">
+                    <h2>Bienvenido</h2>
+                    <p>Ingresa los datos requeridos para crear una cuenta</p>
+                    <p>¿Ya tienes una cuenta? inicia sesión</p>
+                    <button id="btn-register"><a class="btn-ref" href="./index.html">Iniciar sesión</a></button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
