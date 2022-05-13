6f0fcd3dbf41ab54e50e250e19f29cc5470381ee
StephanyMCG <70902398+StephanyMCG@users.noreply.github.com>
2022-05-13 13:29:20 -0500

    Update Cards.php

--- resources/views/Cards.php
+++ resources/views/Cards.php
@@ -1,1 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles2.css">
+    <title>Document</title>
+</head>
+<body> 
+    <header>
+        <div class="container">
+            <section>
+        <nav class="inicio">
+            <a class="enlace">
+                <img src="../img/logo_princi.png" class="logo" alt="">
+            </a>
+            
+                
+            <ul class="ulmenu">
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="clientes.html">Clientes</a></li>
+                <li><a href="clientesP.html">Clientes Potenciales</a></li>
+                <li><a href="#" id="on"><img src="../img/on.png" alt=""></a></li>
+            </ul>
+        
+       
+        </nav>
+        </section>
+    </div>
+    </header>
+    <section>
+        <div>
+            <form action="" class="formulario">
+                <div class="inputs">
+                    <input type="image" id="imagen" name="imagen" src="../img/image.png">
+                    <input type="text" id="name" name="nombre" placeholder="Nombre">
+                    <input type="text" id="phone" name="telefono" placeholder="Teléfono">
+                    <input type="email" id="mail" name="mail" placeholder="Correo Electrónico">
+                    <input type="text" id="empresa" name="empresa" placeholder="Empresa">
+                    <input type="text" id="card" name="card" placeholder="Tarjeta de crédito">
+                    <input type="text" id="fecha" name="fechaV" placeholder="Fecha vencimiento">
+                    <input type="text" id="cvv" name="cvv" placeholder="CVV">
+                    <input type="text" id="desc" name="desc" placeholder="Descripción del cliente">
+                    <button class="boton">Registrar</button>
+                </div>
+                    
+            </form>
+        </div>
+    </section>
+   
 
+</body>
+</html>
