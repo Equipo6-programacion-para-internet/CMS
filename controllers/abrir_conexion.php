e543c265ce8df0059a1d3e3e1309b0919bc3f4b8
Aarón Yael Carranza Vera <70918693+ANYL2000@users.noreply.github.com>
2022-05-13 18:28:56 -0500

    Update abrir_conexion.php

--- controllers/abrir_conexion.php
+++ controllers/abrir_conexion.php
@@ -1,15 +1,11 @@
 <?php
 
                      // usuario de base de datos en hosting
-/*$host = 'localhost';
-$db_username = 'id18051717_admin';
-$db_password = 'Equipo-6-Base-de-datos';
-$db_name = 'id18051717_agenda';
-*/
+
 $host = 'localhost';                //usuario de base de datos en localhost
 $db_username = 'root';
 $db_password = 'root';
-$db_name = 'cms';
+$db_name = 'dclient';
 
 
 $conexion = mysqli_connect($host,$db_username,$db_password,$db_name) or die ("Ha fallado la conexion".mysql_error());
