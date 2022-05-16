e3f15e8178e0d86bcd8ccbbf95ef2d1ebce0707d
Admin Gutierrez <70972423+Admin18-03@users.noreply.github.com>
2022-05-16 15:16:02 -0500

    Ya se agregan los registros a la base de datos

--- resources/views/Cards.php
+++ resources/views/Cards.php
@@ -19,9 +19,9 @@
             
                 
             <ul class="ulmenu">
-                <li><a href="index.html">Inicio</a></li>
-                <li><a href="clientes.html">Clientes</a></li>
-                <li><a href="clientesP.html">Clientes Potenciales</a></li>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="clientes.php">Clientes</a></li>
+                <li><a href="clientesP.php">Clientes Potenciales</a></li>
                 <li><a href="#" id="on"><img src="../img/on.png" alt=""></a></li>
             </ul>
         
@@ -32,7 +32,7 @@
     </header>
     <section>
         <div>
-            <form action="" class="formulario">
+            <form action="" class="formulario" action="Cards.php" method="POST" >
                 <div class="inputs">
                     <input type="image" id="imagen" name="imagen" src="../img/image.png">
                     <input type="text" id="name" name="nombre" placeholder="Nombre">
@@ -43,13 +43,37 @@
                     <input type="text" id="fecha" name="fechaV" placeholder="Fecha vencimiento">
                     <input type="text" id="cvv" name="cvv" placeholder="CVV">
                     <input type="text" id="desc" name="desc" placeholder="Descripción del cliente">
-                    <button class="boton">Registrar</button>
+                    <input type="submit" class="btn btn-success" value="Registrar" id="btn" name="btn">
+                    <button class="boton" id="btn" name="btn">Registrar</button>
+                    <?php
+        include "../../controllers/abrir_conexion.php";
+        include "../../controllers/Hash.php";
+        if(isset($_POST['btn'])){
+            $subs_imagen = $_POST['imagen'];
+            $subs_name = $_POST['nombre'];
+            $subs_telefono = $_POST['telefono'];
+            $subs_email = $_POST['email'];
+            $subs_empresa = $_POST['empresa'];
+            $subs_card = $_POST['card'];
+            $subs_fechaV = $_POST['fechaV'];
+            $subs_cvv = $_POST['cvv'];
+            $subs_dir = $_POST['direccion'];
+            $subs_desc = $_POST['desc'];
+
+        
+            $conexion->query("INSERT INTO clientes (Foto,Nombres,Apellidos,Telefono,Empresa,Tarjeta,FechaT,CVV,DireccionF,Descripcion) values ('$subs_imagen','$subs_name','$subs_last','$subs_telefono','$subs_empresa','$subs_card','$subs_fechaV','$subs_cvv','$subs_dir','$subs_desc')");
+            
+    
+    
+          }
+    
+    ?>
                 </div>
                     
             </form>
         </div>
     </section>
-   
 
+    
 </body>
 </html>
