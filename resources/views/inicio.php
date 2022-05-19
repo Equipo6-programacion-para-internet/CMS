1e418a80edbf8ef514403ecbcce97f0613892f4c
StephanyMCG <70902398+StephanyMCG@users.noreply.github.com>
2022-05-18 21:21:40 -0500

    Update inicio.php

--- resources/views/inicio.php
+++ resources/views/inicio.php
@@ -32,13 +32,19 @@
         </section>
         <div class="Dclient">
             <h1>DClient<br></h1>
+    
             <p>DClient es un CMS enfocado a un directorio<br>de clientes. Con esta aplicación web puedes<br>almacenar de forma segura los datos de tus<br>clientes y poder tener el control sobre su<br>información.</p>
-            <img src="../img/DClient.jpg" alt="">
+            <div >
+            <img id="img1" src="../img/DClient.jpg" alt="">
+            </div>
         </div>
         <div class="caracteristicas">
-        <h1>DClient<br></h1>
-            <p>DClient te ofrece la oportunidad de visualizar<br>clientes, crear, mostrar, actualizar y eliminar<br>con base a la información de ellos.<br>Cada operación que se realice requiere credenciales de nivel de usuario<br>y así se puedan efectuar dichos movimientos.<br>Es fácil de usar, cómodo, seguro, agradable<br>a la vista y con todo lo necesario y básico<br>de una agenda, pero con un plus.</p>
-            <img src="../img/caracteristicas.jpg" alt="">
+        <h1>Características<br></h1>
+        
+            <p>DClient te ofrece la oportunidad de visualizar<br>clientes, crear, mostrar, actualizar y eliminar<br>con base a la información de ellos. Cada operación<br>que se realice requiere credenciales de nivel<br>de usuario y así se puedan efectuar dichos<br>movimientos. Es fácil de usar, cómodo, seguro, <br>agradable a la vista y con todo lo necesario <br>y básico de una agenda, pero con un plus.</p>
+            <div class="img">
+            <img id="img2" src="../img/caracteristicas.jpg" alt="">
+            </div>
         </div>
         <div class="equipo">
             <h1>Equipo desarrollador</h1>
