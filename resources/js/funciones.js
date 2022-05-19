75a00754521958ad3e30537ac707094fa92d8881
Aarón Yael Carranza Vera <70918693+ANYL2000@users.noreply.github.com>
2022-05-18 19:53:00 -0500

    Se agrego el archivo jquery y una de funciones
    El archivo de funciones.js son para poder usar cada función en la que se pueda imprimir un mensaje de error sweetalert

---
+++ resources/js/funciones.js
@@ -0,0 +1,95 @@
+function enterClick(x,y) {
+    var input = document.getElementById(x);
+  
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function(event) {
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById(y).click();
+      }
+    });
+  }
+
+function alerta2(t) {
+    Swal.fire({
+  
+      title: '¡Exito!',
+      text: t,
+      icon: 'success',
+      allowOutsideClick: false
+    })
+  }
+  
+  function alertae(t) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error...',
+      text: t
+    })
+  }
+  
+  function alertaSes(t) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error...',
+      text: t,
+      allowOutsideClick: false
+    }).then(function() {
+      window.location.href="login";
+    })
+  }
+  
+  
+  function alertae2(t) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Error...',
+      text: t,
+      allowOutsideClick: false
+    }).then(function() {
+      location.reload(true);
+    })
+  }
+  
+  function simpleAlert(t) {
+    Swal.fire(t)
+  }
+  
+  function alerta(t) {
+    Swal.fire({
+  
+      title: '¡Exito!',
+      text: t,
+      icon: 'success',
+      allowOutsideClick: false
+    }).then(function() {
+      location.reload(true);
+    })
+  }
+  
+  function alertaNoti(t) {
+    Swal.fire({
+      position: 'bottom-end',
+      icon: 'success',
+      title: 'Exito',
+      text: t,
+      showConfirmButton: false,
+      timer: 800,
+      allowOutsideClick: true
+    })
+  }
+  
+  function alertaeNoti(t) {
+    Swal.fire({
+      position: 'bottom-end',
+      icon: 'error',
+      title: 'Error...',
+      text: t,
+      showConfirmButton: false,
+      timer: 800,
+      allowOutsideClick: true
+    })
+  }
