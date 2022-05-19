4c27877f30dff5cb5cafff8613d6ef5c78645dfb
StephanyMCG <70902398+StephanyMCG@users.noreply.github.com>
2022-05-18 20:43:20 -0500

    Página inicial del cms, caracteísticas, quué es y equipo

---
+++ resources/views/inicio.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles5.css">
+    <link rel="icon" href="../img/buscar.png">
+    <title>Ver</title>
+</head>
+<body> 
+    <header>
+        <div class="container">
+            <section>
+        <nav class="inicio">
+            <a class="enlace">
+                <img src="../img/logo_princi.png" class="logo" alt="">
+            </a>
+            
+                
+            <ul class="ulmenu">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="clientes.php">Clientes</a></li>
+                <li><a href="clientesP.php">Clientes Potenciales</a></li>
+                <li><a href="#" id="on"><img src="../img/on.png" alt=""></a></li>
+                <li><a href="#" id="on"><img src="../img/cliente.png" alt=""></a></li>
+                <li><a href="">Username</a></li>
+            </ul>
+        
+       
+        </nav>
+        </section>
+        <div class="Dclient">
+            <h1>DClient<br></h1>
+            <p>DClient es un CMS enfocado a un directorio<br>de clientes. Con esta aplicación web puedes<br>almacenar de forma segura los datos de tus<br>clientes y poder tener el control sobre su<br>información.</p>
+            <img src="../img/DClient.jpg" alt="">
+        </div>
+        <div class="caracteristicas">
+        <h1>DClient<br></h1>
+            <p>DClient te ofrece la oportunidad de visualizar<br>clientes, crear, mostrar, actualizar y eliminar<br>con base a la información de ellos.<br>Cada operación que se realice requiere credenciales de nivel de usuario<br>y así se puedan efectuar dichos movimientos.<br>Es fácil de usar, cómodo, seguro, agradable<br>a la vista y con todo lo necesario y básico<br>de una agenda, pero con un plus.</p>
+            <img src="../img/caracteristicas.jpg" alt="">
+        </div>
+        <div class="equipo">
+            <h1>Equipo desarrollador</h1>
+            <h5>Alexis de Jesús Aguirre Lara</h5>
+            <h5>Aarón Yael Carranza Vera</h5>
+            <h5>Stephany Michelle Cortés González</h5>
+            <h5>Juan Pedro Domíngez Padilla</h5>
+            <h5>Admin Yair Gutiérrez Domínguez</h5>
+            <h5>Jazmín Alejandra Lomelí Zermeño</h5>
+        </div>
+
+        
+</body>
+</html>
