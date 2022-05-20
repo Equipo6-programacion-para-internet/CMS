cf91224b604a109381858e65edb71bd1fca90cdc
Aarón Yael Carranza Vera <70918693+ANYL2000@users.noreply.github.com>
2022-05-20 14:45:31 -0500

    Se agrego archivo Buscar.js
    Este es el archivo en el que hace la búsqueda de acuerdo a lo que se captura desde el campo de texto de Buscar.php

---
+++ resources/js/Buscar.js
@@ -0,0 +1,29 @@
+//$(buscar_datos());
+
+
+function buscar_datos(consulta){
+    $.ajax({
+url: '../views/Esqueleto_Buscar.php',
+type: 'POST',
+dataType: 'html',
+data: {consulta: consulta},
+
+    })
+    .done(function(respuesta){
+            $('#datos').html(respuesta);
+    })
+    .fail(function(){
+
+    })
+   
+}
+
+
+$(document).on('keyup','#buscar',function(){
+var valor = $(this).val();
+if(valor!=""){
+buscar_datos(valor);
+}else{
+    buscar_datos();
+}
+});
