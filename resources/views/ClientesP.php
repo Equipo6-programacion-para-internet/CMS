d79b70c69b803bb607c07ddfe3c375e4129fa344
Admin Gutierrez <70972423+Admin18-03@users.noreply.github.com>
2022-05-20 23:54:45 -0500

    Muestra los clientes extrayendo de la base de datos

--- resources/views/ClientesP.php
+++ resources/views/ClientesP.php
@@ -6,7 +6,7 @@ $conexion = connect($server, $port, $db, $user, $pass);
 if(!$conexion){
    header('Location: Login.php');
 }
-$query =$conexion->prepare("SELECT Nombres, Apellidos, Telefono, DireccionF 
+$query =$conexion->prepare("SELECT Nombres, Apellidos, Telefono, DireccionF, Descripcion
 FROM clientes WHERE idClientes>1;");
 
 $query->execute();
@@ -19,47 +19,56 @@ $resultado =$query->fetchAll();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style7.css">
     <link rel="stylesheet" href="../css/styles3.css">
     <link rel="icon" href="../img/clienteP.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  
     <title>Clientes</title>
 </head>
-<body>
-    <header>
+<header>
         <div class="container">
-                <section>
-            <nav class="inicio">
-                <a class="enlace">
-                    <img src="../img/logo_princi.png" class="logo" alt="">
-                </a>
-                
-                    
-                <ul class="ulmenu">
-                    <li><a href="index.html">Inicio</a></li>
-                    <li><a href="clientes.html">Clientes</a></li>
-                    <li><a href="clientesP.html">Clientes Potenciales</a></li>
-                    <li><a href="#" id="on"><img src="../img/on.png" alt=""></a></li>
-                </ul>
+            <section>
+        <nav class="inicio">
+            <a class="enlace">
+                <img src="../img/logo_princi.png" class="logo" alt="">
+            </a>
             
+                
+            <ul class="ulmenu">
+                <li><a href="inicio.php">Inicio</a></li>
+                <li><a href="clientes.php">Clientes</a></li>
+                <li><a href="clientesP.php">Clientes Potenciales</a></li>
+                <li><a href="#" id="on"><img src="../img/on.png" alt=""></a></li>
+            </ul>
         
-            </nav>
-            </section>
-        </div>
-        </header>
-        <section>
+       
+        </nav>
+        </section>
+    </div>
+    </header>
+    <ul class="tilesWrap">
       <?php foreach($resultado as $cliente) :?>
-            <div class="cliente">
-                <h3> 
-                    <?php echo $cliente['Nombres']?> 
-                    <?php echo $cliente['Apellidos']?> 
-                </h3>
-                <p><?php echo $cliente['Telefono']?></p>
-                <p><?php echo $cliente['DireccionF']?></p>
-            </div>           
-        <?php endforeach; ?>
-  </section>
-
+   
+	    <li>
+		    <h2><img src="../img/image.png" alt=""></h2>
+		    <h3>
+                <?php echo $cliente['Nombres']?> 
+                <?php echo $cliente['Apellidos']?>
+            </h3>
+            <p>
+                    <?php echo $cliente['Telefono']?>
+                    <?php echo $cliente['DireccionF']?>
+            </p>
+		    <p>
+                    <?php echo $cliente['Descripcion']?>
+		    </p>
+		    <button>Ver mas</button>
+	    </li>
     
-    </body>
-
-</html>
+        
+        <?php endforeach; ?>
+        </ul>
+</html> 
 <?php $conexion = null;?>
