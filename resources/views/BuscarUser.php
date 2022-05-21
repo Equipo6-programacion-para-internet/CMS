3d531619dd253cdbfd81e33d42c01be9c3ac8fbb
StephanyMCG <70902398+StephanyMCG@users.noreply.github.com>
2022-05-21 09:48:16 -0500

    Create BuscarUser.php

---
+++ resources/views/BuscarUser.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles7.css">
+    <link rel="icon" href="../img/buscar.png">
+    <title>Ver</title>
+</head>
+<body> 
+    <header>
+        <div class="container">
+            <section>
+        <nav class="inicio">
+            <a class="enlace">
+                <img src="../img/logo_princi.png" class="logo" alt="">
+            </a>
+            
+                
+            <ul class="ulmenu">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="clientes.php">Clientes</a></li>
+                <li><a href="clientesP.php">Clientes Potenciales</a></li>
+                <li><a href="#" id="on"><img src="../img/on.png" alt=""></a></li>
+                <li><a href="#" id="on"><img src="../img/cliente.png" alt=""></a></li>
+                <li><a href="">Username</a></li>
+            </ul>
+        
+       
+        </nav>
+        </section>
+        <div class="container1">
+            <h1>Usuarios</h1>
+        
+        
+        <div class="container2">
+            <input type="text" id="buscar" placeholder="buscar" >
+        </div>
+    
+        <footer>
+            <h4>Texto de relleno para cuando se me ocurra que poner</h4>
+            <h4>&#169 Dclient</h4>
+        </footer>
+        
+</body>
+</html>
