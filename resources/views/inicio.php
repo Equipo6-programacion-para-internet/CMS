2061893842ce770c55e3d7baaf0118730144c047
Juan Pedro <juan.dominguez2763@gmail.alumnos.udg.mx>
2022-05-22 09:27:57 -0500

    Correccion vista inicio.php

--- resources/views/inicio.php
+++ resources/views/inicio.php
@@ -9,55 +9,82 @@
     <title>Inicio</title>
 </head>
 <body>
-    <header>
-        <div class="container"> <!-- Color del header -->
-            <nav class="inicio">
-                <a class="enlace">
-                    <img src="../img/logo_princi.png" class="logo" alt="">  <!-- Icono DClient  -->
-                </a>
-                
-            <ul class="ulmenu">
-                <li><a href="inicio.php">Inicio</a></li>
-                <li><a href="Clientes.php">Clientes</a></li>
-                <li><a href="ClientesP.php">Clientes Potenciales</a></li>
-                <li><a href="#" id="on"><img src="../img/on.png" alt=""></a></li>
-                <li><a href="#" id="on"><img src="../img/cliente.png" alt=""></a></li>
-                <li><a href="">Username</a></li>
-            </ul>
-            </nav>
-        </div>
-    </header>
+    <div class="container-header">
+        <header >
+            <nav>
+                <div class="logo">
+                    <img  src="../img/logo.png" alt="logo">
+                </div>
+                <div class="elementos">
+                    <ul>
+                        <li><a class="active" href="#">Clientes</a></li>
+                        <li><a href="#">Clientes potenciales</a></li>
+                        <li><a href="#">Registrarse</a></li>
+                    </ul>
+                </div>
+                </nav>
+            
+        </header>
+    </div>
 
-      <seccion>
-          <div class="Dclient">
-             <h1>DClient<br></h1>
-              <p>DClient es un CMS enfocado a un directorio<br>de clientes. Con esta aplicación web puedes<br>almacenar de forma segura los datos de tus<br>clientes y poder tener el control sobre su<br>información.</p>
-            <div >
-               <img id="img1" src="../img/DClient.jpg" alt="">
-             </div>
+    
+        <div class="container">
+        <section>
+            <div class="Dclient">
+                <div class="col-left">
+                    <div class="information">
+                        <h1>DClient</h1>
+                         <p>DClient es un CMS enfocado a un directorio<br>de clientes. Con esta aplicación web puedes<br>almacenar de forma segura los datos de tus<br>clientes y poder tener el control sobre su<br>información.</p>
+                    </div>
+                </div>
+              <div class="col-rigth">
+                  <div class="container-img">
+                      <img id="img1" src="../img/DClient.jpg" alt="">
+
+                  </div>
+               </div>
+            </div>
+  
+  
+  
+        </section>
+
+        <section>
+  
+          <div class="caracteristicas direction-reverse">
+
+            <div class="col-left">
+                <h1>Características<br></h1>                
+                <p>DClient te ofrece la oportunidad de visualizar<br>clientes, crear, mostrar, actualizar y eliminar<br>con base a la información de ellos. Cada operación<br>que se realice requiere credenciales de nivel<br>de usuario y así se puedan efectuar dichos<br>movimientos. Es fácil de usar, cómodo, seguro, <br>agradable a la vista y con todo lo necesario <br>y básico de una agenda, pero con un plus.</p>
+            </div>
+            <div class="col-rigth">
+                <div class="container-img">
+
+              <img id="img2" src="../img/caracteristicas.jpg" alt="">
+                </div>
+
+            </div>
           </div>
 
 
+        </section>
 
-      </seccion>
+        <section>
+          <div class="equipo">
+              <h1>Equipo desarrollador</h1>
+              <h5>Alexis de Jesús Aguirre Lara</h5>
+              <h5>Aarón Yael Carranza Vera</h5>
+              <h5>Stephany Michelle Cortés González</h5>
+              <h5>Juan Pedro Domíngez Padilla</h5>
+              <h5>Admin Yair Gutiérrez Domínguez</h5>
+              <h5>Jazmín Alejandra Lomelí Zermeño</h5>
+          </div>
+          
 
-        <div class="caracteristicas">
-        <h1>Características<br></h1>
-        
-            <p>DClient te ofrece la oportunidad de visualizar<br>clientes, crear, mostrar, actualizar y eliminar<br>con base a la información de ellos. Cada operación<br>que se realice requiere credenciales de nivel<br>de usuario y así se puedan efectuar dichos<br>movimientos. Es fácil de usar, cómodo, seguro, <br>agradable a la vista y con todo lo necesario <br>y básico de una agenda, pero con un plus.</p>
-            <div class="img">
-            <img id="img2" src="../img/caracteristicas.jpg" alt="">
-            </div>
-        </div>
-        <div class="equipo">
-            <h1>Equipo desarrollador</h1>
-            <h5>Alexis de Jesús Aguirre Lara</h5>
-            <h5>Aarón Yael Carranza Vera</h5>
-            <h5>Stephany Michelle Cortés González</h5>
-            <h5>Juan Pedro Domíngez Padilla</h5>
-            <h5>Admin Yair Gutiérrez Domínguez</h5>
-            <h5>Jazmín Alejandra Lomelí Zermeño</h5>
-        </div>
+        </section>
+
+    </div>
+
 
         <footer>
 
@@ -68,7 +95,7 @@
                     <li><a href=" https://web.whatsapp.com" target="_blank"><img src="../img/whatsapp.png" alt=""></a> </li>
                     <li><a href=" https://www.instagram.com" target="_blank"> <img src="../img/instagram.png" alt=""></a></li>
                     <li><a href=" https://www.facebook.com/" target="_blank"> <img src="../img/facebook.png" alt=""> </li>
-                    <li><a href=" https://mx.linkedin.com" target="_blank"><img src="../img/linkedin.png" alt=""></a> </li>                  
+                    <li><a href=" https://mx.linkedin.com" target="_blank"><img src="../img/linkedin.png" alt=""></a></li>                  
                 </ul>
             </div>
             <h5> &#169 Dclient</h5>
@@ -78,5 +105,8 @@
         
 </body>
 </html>
+
+    
+
 
     
