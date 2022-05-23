117faf177eeee84bc7866abdd55939094c38b9f1
Aarón Yael Carranza Vera <70918693+ANYL2000@users.noreply.github.com>
2022-05-22 23:39:55 -0500

    Se agregaron más validaciones de campos

--- views/ClientesStruct.php
+++ views/ClientesStruct.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/styles3.css">
-    <link rel="icon" href="../img/cliente.png">
+    <link rel="stylesheet" href="../public/css/styles3.css">
+    <link rel="icon" href="../public/img/cliente.png">
     <title>Document</title>
 </head>
 <body> 
@@ -13,15 +13,15 @@
         <div class="container"> <!-- Color del header -->
             <nav class="inicio">
                 <a class="enlace">
-                    <img src="../img/logo_princi.png" class="logo" alt="">  <!-- Icono DClient  -->
+                    <img src="../public/img/logo_princi.png" class="logo" alt="">  <!-- Icono DClient  -->
                 </a>
                 
             <ul class="ulmenu">
                 <li><a href="inicio.php">Inicio</a></li>
                 <li><a href="Clientes.php">Clientes</a></li>
                 <li><a href="ClientesP.php">Clientes Potenciales</a></li>
-                <li><a href="#" id="on"><img src="../img/on.png" alt=""></a></li>
-                <li><a href="#" id="on"><img src="../img/cliente.png" alt=""></a></li>
+                <li><a href="#" id="on"><img src="../public/img/on.png" alt=""></a></li>
+                <li><a href="#" id="on"><img src="../public/img/cliente.png" alt=""></a></li>
                 <li><a href="">Username</a></li>
             </ul>
             </nav>
@@ -39,10 +39,9 @@
                 <p>Foto de perfil</p>
                
             </div>-->
-            <div class="descripcion">
-                <p>Descripción</p>
-                <input id="descripcion" type="text" name="desc">
-            </div>
+           
+
+
         </div>
    </section>
     <section>
@@ -52,12 +51,12 @@
                 <form class="formulario" action="ClientesStruct.php" method="POST" enctype="multipart/form-data">
 
                 <div class="foto-cliente">
-                <img class="entrada_texto"  id="imagen" type="image" name="imagen" src="../img/image.png">
+                <img class="entrada_texto"  id="imagen" type="image" name="imagen" src="../public/img/image.png">
 
                
 <div class="custom-input-file col-md-6 col-sm-6 col-xs-6">
 <input type="file" class="input-file" value="" id="subir_imagen" name="subir_imagen" accept="image/png, image/jpeg, image/jpg">
-<img class="subir_foto" src="../img/subir_foto.png" alt="">
+<img class="subir_foto" src="../public/img/subir_foto.png" alt="">
 </div>
 </div>
                     <label for="name" >Nombre<br>
@@ -84,8 +83,13 @@
                     <label  for="cvv">CVV<br>
                     <input class="entrada_texto" type="text" id="cvv" name="cvv"></label>
 
-                    <label  for="factura">Dirección de facturación <br>
+                    <label  for="factura" class="label_direc">Dirección de facturación <br>
                     <input class="entrada_texto" type="text" id="direccion" name="direccion"></label>
+
+                  
+                <label id="descripcion_label" for="desc">Descripción<br>
+                <input class="entrada_texto" id="descripcion" type="text" name="desc"></label>
+         
 
                     <input type="submit" class="boton" id="btn" name="btn" value="Guardar">
                 </form>
@@ -93,11 +97,14 @@
 
         </div>
     </section>
-    <script src="../js/jquery-3.6.0.min.js"></script>
-    <script src="../js/previsualizar_fotoperfil.js"></script>
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="../resources/js/funciones.js"></script>  
+    <script src="../resources/js/jquery-3.6.0.min.js"></script>
+    <script src="../resources/js/previsualizar_fotoperfil.js"></script>
     <?php
-        include "../../controllers/abrir_conexion.php";
-        include "../../controllers/Hash.php";
+        include "../controllers/abrir_conexion.php";
+        include "../controllers/Hash.php";
         if(isset($_POST['btn'])){
            // $subs_imagen = $_POST['subir_imagen'];
             $subs_name = $_POST['nombre'];
@@ -114,7 +121,7 @@
 $tipo = $_FILES['subir_imagen']['type'];         
 $foto=$_FILES['subir_imagen']['name'];
 $ruta=$_FILES['subir_imagen']['tmp_name'];
-$destino="../fotoClientes/".$foto;
+$destino="../resources/fotoClientes/".$foto;
 
 /*echo 'Nombre'.$subs_name; 
 echo 'Apellido'.$subs_last ;
@@ -127,6 +134,30 @@ echo 'CVV'.$subs_cvv ;
 echo 'Direccion'.$subs_dir ;
 echo 'Descripcion'.$subs_desc ;*/
 
+$validar_campo_vacio = "";
+
+if($subs_name==""){
+    $validar_campo_vacio="El campo del Nombre esta vacio";
+
+}else if($subs_last==""){
+
+    $validar_campo_vacio="El campo del Apellido esta vacio";
+
+}else if($subs_email==""){
+
+    $validar_campo_vacio="El campo del Correo Electrónico esta vacio";
+
+}else if($subs_telefono==""){
+
+    $validar_campo_vacio="El campo del Número de Teléfono esta vacio";
+
+}else if($subs_dir==""){
+
+    $validar_campo_vacio="El campo de la Dirección de facturación esta vacio";
+}
+
+if($subs_name!=""&&$subs_last!=""&&$subs_email!=""&&$subs_telefono!=""&&$subs_dir!=""){
+
 $extensiones = array(0=>'image/jpg',1=>'image/jpeg',2=>'image/png');
 
 if ( in_array($_FILES['subir_imagen']['type'], $extensiones) || $ruta=="" ) {
@@ -135,17 +166,29 @@ if ( in_array($_FILES['subir_imagen']['type'], $extensiones) || $ruta=="" ) {
         copy($ruta,$destino);
         }else{
             $destino="";
-            $destino.="../fotoClientes/foto1.jpg";
+            $destino.="../resources/fotoClientes/foto1.jpg";
         }
-
-    $conexion->query("INSERT INTO clientes (Foto,Nombres,Apellidos,Telefono,Empresa,Tarjeta,FechaT,CVV,DireccionF,Descripcion) values ('$destino','$subs_name','$subs_last','$subs_telefono','$subs_empresa','$subs_card','$subs_fechaV','$subs_cvv','$subs_dir','$subs_desc')");
-            
+if(strpos($foto, " ")){
+    echo '<script>alertaeNoti("El nombre del archivo contiene espacios vacios")</script>';
 
 }else{
-    echo '<script>alert("La extension del archivo es incorrecta")</script>';
+    $conexion->query("INSERT INTO clientes (Foto,Nombres,Apellidos,Email,Telefono,Empresa,Tarjeta,FechaT,CVV,DireccionF,Descripcion) values ('$destino','$subs_name','$subs_last','$subs_email','$subs_telefono','$subs_empresa','$subs_card','$subs_fechaV','$subs_cvv','$subs_dir','$subs_desc')");
+       
+    if($conexion){
+        echo '<script>alertaNoti("Se ha registrado el cliente con exito")</script>';
+    }
+}
+}else{
+    echo '<script>alertaeNoti("La extension del archivo es incorrecta")</script>';
 }
 
-    
+}else{
+
+
+    echo "<script>alertaeNoti('$validar_campo_vacio')</script>";
+
+   // $validar_campo_vacio = "";
+}  
           }
     
     ?>
@@ -158,10 +201,10 @@ if ( in_array($_FILES['subir_imagen']['type'], $extensiones) || $ruta=="" ) {
 
             <div class="lista_footer">                  
                     <ul class="foot">
-                        <li><a href=" https://web.whatsapp.com" target="_blank"><img src="../img/whatsapp.png" alt=""></a> </li>
-                        <li><a href=" https://www.instagram.com" target="_blank"> <img src="../img/instagram.png" alt=""></a></li>
-                        <li><a href=" https://www.facebook.com/" target="_blank"> <img src="../img/facebook.png" alt=""> </li>
-                        <li><a href=" https://mx.linkedin.com" target="_blank"><img src="../img/linkedin.png" alt=""></a> </li>                  
+                        <li><a href=" https://web.whatsapp.com" target="_blank"><img src="../public/img/whatsapp.png" alt=""></a> </li>
+                        <li><a href=" https://www.instagram.com" target="_blank"> <img src="../public/img/instagram.png" alt=""></a></li>
+                        <li><a href=" https://www.facebook.com/" target="_blank"> <img src="../public/img/facebook.png" alt=""> </li>
+                        <li><a href=" https://mx.linkedin.com" target="_blank"><img src="../public/img/linkedin.png" alt=""></a> </li>                  
                     </ul>
                 </div>
                 <h5> &#169 Dclient</h5>
@@ -170,3 +213,4 @@ if ( in_array($_FILES['subir_imagen']['type'], $extensiones) || $ruta=="" ) {
 
 </body>
 </html>
+
